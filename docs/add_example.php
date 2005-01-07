ab1ba176effdc2fa24ae1cb83e8eade70a423d6f
Lorenzo Alberton <quipo@php.net>
2005-01-07 13:47:20 +0000

    - use single quotes - fix wrong condition (bug #3131)
    git-svn-id: https://svn.php.net/repository/pear/packages/Mail_Queue/trunk@176618 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/add_example.php
+++ docs/add_example.php
@@ -1,28 +1,30 @@
 <?php
 
-require_once ( "base.php" );
+require_once 'base.php';
 $mail_queue =& new Mail_Queue($db_options, $mail_options);
 
 $from = 'user@server.com';
-$from_name = "Chief";
+$from_name = 'Chief';
 
-$recipient = "user2@server.com";
-$recipient_name = "admin";
+$recipient = 'user2@server.com';
+$recipient_name = 'admin';
 $message = 'Hi! This is test message!! :)';
-$from_params = empty($from_name) ? '"'.$from_name.'" <'.$from.'>' : '<'.$from.'>';
-$recipient_params = empty($recipient_name) ? '"'.$recipient_name.'" <'.$recipient.'>' : '<'.$recipient.'>';
-$hdrs = array( 'From'    => $from_params,
-               'To'      => $recipient_params,
-               'Subject' => "test message body"  );
+$from_params = !empty($from_name) ? '"'.$from_name.'" <'.$from.'>' : '<'.$from.'>';
+$recipient_params = !empty($recipient_name) ? '"'.$recipient_name.'" <'.$recipient.'>' : '<'.$recipient.'>';
+$hdrs = array(
+    'From'    => $from_params,
+    'To'      => $recipient_params,
+    'Subject' => 'test message body',
+);
 
-$mime =& new Mail_mime();
+$mime =& new Mail_mime;
 $mime->setTXTBody($message);
 $body = $mime->get();
 $hdrs = $mime->headers($hdrs);
 
 
 /* Put message to queue */
-$mail_queue->put( $from, $recipient, $hdrs, $body );
+$mail_queue->put($from, $recipient, $hdrs, $body);
 
 
 /* Also you could put this msg in more advanced mode */
@@ -36,6 +38,6 @@ $delete_after_send = false;
 // if you backup some mails in db you could group them later by the user identifier, for example
 $id_user = 7;
 
-$mail_queue->put( $from, $recipient, $hdrs, $body, $seconds_to_send, $delete_after_send, $id_user );
+$mail_queue->put($from, $recipient, $hdrs, $body, $seconds_to_send, $delete_after_send, $id_user);
 
 ?>
