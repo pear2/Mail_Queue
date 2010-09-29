06739e88495bfa9b0ec85c6dc2959d62c91650e7
Till Klampaeckel <till@php.net>
2010-09-29 16:48:10 +0000

    * working on a test for sendMailsInQueue()
    git-svn-id: https://svn.php.net/repository/pear/packages/Mail_Queue/trunk@303874 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Mail/QueueTest.php
+++ tests/Mail/QueueTest.php
@@ -31,4 +31,39 @@ class Mail_QueueTest extends Mail_QueueAbstract
         $this->assertContains('no such message', $status->getDebugInfo());
         $this->assertTrue(($status instanceof Mail_Queue_Error));
     }
+
+    /**
+     * Queue two emails - to be send right away.
+     *
+     * @return void
+     */
+    public function testSendMailsInQueue()
+    {
+        $this->markTestIncomplete("Doesn't send yet, need to doublecheck my table definition.");
+
+        $id_user      = 1;
+        $sender       = 'testsuite@example.org';
+        $recipient    = 'testcase@example.org';
+        $headers      = array('X-TestSuite' => 1);
+        $body         = 'Lorem ipsum';
+
+        $mailId1 = $this->queue->put($sender, $recipient, $headers, $body);
+
+        $id_user      = 1;
+        $sender       = 'testsuite@example.org';
+        $recipient    = 'testcase@example.org';
+        $headers      = array('X-TestSuite' => 2);
+        $body         = 'Lorem ipsum sit dolor';
+
+        $mailId2 = $this->queue->put($sender, $recipient, $headers, $body);
+
+        $this->assertEquals(2, $this->queue->getQueueCount());
+
+        $status = $this->queue->sendMailsInQueue();
+        if (Pear::isError($status)) {
+            $this->fail("Error sending emails: {$status->getMessage()}.");
+        }
+        $this->assertTrue($status);
+        $this->assertEquals(0, $this->queue->getQueueCount());
+    }
 }
